078be1a1d910e4d67215664d33011b112bbb9fc0
Mikhail Kryuchkov <kryuchkovm@outlook.com>
2021-07-25 22:10:27 +0300

    [+] Input handlers: save question, save answer

---
+++ TgBotPillar.Bot.Input/Handlers/SaveAnswerHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TgBotPillar.Bot.Input.Defaults;
+using TgBotPillar.Core.Interfaces;
+using TgBotPillar.Core.Model;
+
+namespace TgBotPillar.Bot.Input.Handlers
+{
+    public class SaveAnswerHandler : IInputHandler
+    {
+        public string Name => "save_answer";
+
+        public async Task<string> Handle(IStorageService storageService, IDictionary<string, string> parameters,
+            IDialogContext context, string text)
+        {
+            await storageService.SaveQuestion(
+                context.ChatId,
+                parameters[HandlerParameter.QuestionType],
+                text);
+
+            return text;
+        }
+    }
+}
---
+++ TgBotPillar.Bot.Input/Handlers/SaveQuestionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TgBotPillar.Bot.Input.Defaults;
+using TgBotPillar.Core.Interfaces;
+using TgBotPillar.Core.Model;
+
+namespace TgBotPillar.Bot.Input.Handlers
+{
+    public class SaveQuestionHandler : IInputHandler
+    {
+        public string Name => "save_question";
+        public async Task<string> Handle(IStorageService storageService,
+            IDictionary<string, string> parameters, IDialogContext context,
+            string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return HandlerResponse.Error;
+            
+            await storageService.SaveQuestion(
+                context.ChatId,
+                parameters[HandlerParameter.QuestionType],
+                text);
+            return HandlerResponse.Ok;
+        }
+    }
+}
